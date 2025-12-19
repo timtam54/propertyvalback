fdddbe5d77bc7b7a6e9e3873f962aeefc8f1b054
timhams <timtam54@hotmail.com>
2025-12-19 17:15:57 +1000

    Fix TypeScript error: use findOne after insertOne for correct type

--- src/routes/historicSalesWeights.ts
+++ src/routes/historicSalesWeights.ts
@@ -29,7 +29,7 @@ router.get('/', async (req: Request, res: Response) => {
       };
 
       await collection.insertOne(defaultWeights);
-      weights = defaultWeights;
+      weights = await collection.findOne({ id: defaultWeights.id });
       console.log('[Historic Sales Weights] Created default weights');
     }
 
